fc154cf27fa7588c06f023ae77a8b694133f35c6
Swingflip <ryan@ryanhamlin.co.uk>
2020-03-04 12:30:37 +0000

    Added new icon and power fix.

--- classic/psc_eris/media/project_eris/etc/project_eris/SUP/launchers/drastic/launch.sh
+++ classic/psc_eris/media/project_eris/etc/project_eris/SUP/launchers/drastic/launch.sh
@@ -3,4 +3,6 @@ source "/var/volatile/project_eris.cfg"
 cd "/var/volatile/launchtmp"
 chmod +x "drastic"
 echo "launch_StockUI" > "/tmp/launchfilecommand"
+echo -n 2 > "/data/power/disable"
 LD_PRELOAD=./drastic_sdl_remap.so ./drastic > "${RUNTIME_LOG_PATH}/drastic.log" 2>&1
+echo -n 1 > "/data/power/disable"
